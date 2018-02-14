50de05e2010d1d393ade313def212bebb5206e95
igor <oparinufa@gmail.com>
2018-02-14 08:48:42 +0500

    Add redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './createStore';
 
 function HelloWorld() {
   return <div>Hello world!</div>;
 }
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HelloWorld />
+  </Provider>,
+  document.getElementById('root')
+);
