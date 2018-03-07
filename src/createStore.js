3b46579c5f773f069af8dbc867ad23feb0179257
Igor <oparinufa@gmail.com>
2018-03-07 10:13:44 +0500

    add redux-devtools

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './ducks';
 
 
-export default createStore(reducer, applyMiddleware(ReduxThunk));
+export default createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk)),
+);
