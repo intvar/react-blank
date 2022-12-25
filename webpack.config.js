958440a5208c80a2bec6dcecccb27fd663cfbc69
Igor Oparin <ioparin@maxilect.com>
2022-12-25 12:06:57 +0500

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
 const { join } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 module.exports = {
+  mode,
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: join(__dirname, 'build'),
+    clean: true,
   },
   plugins: [
-    new ExtractTextPlugin('styles.css'),
     new webpack.NoEmitOnErrorsPlugin(),
     new HtmlWebpackPlugin({
       template: './src/index.template.html',
       inject: 'body',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: 'babel-loader',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
         exclude: /node_modules/,
       },
       {
         test: /\.css/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
       },
     ],
   },
